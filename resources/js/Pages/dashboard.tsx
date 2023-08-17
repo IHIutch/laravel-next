b82f5555e262034eb7276c4512a300f758616c03
JB Hutch <jbhutch01@gmail.com>
2023-08-17 18:50:15 -0400

    Install react router

--- resources/js/Pages/dashboard.tsx
+++ resources/js/Pages/dashboard.tsx
@@ -1,11 +1,111 @@
+import * as React from "react";
 import { Head } from "@inertiajs/react";
+import {
+    ColumnDef,
+    flexRender,
+    getCoreRowModel,
+    getSortedRowModel,
+    SortingState,
+    useReactTable,
+} from "@tanstack/react-table";
 
-export default function Dashboard({ parcels }: { parcels: any[] }) {
+type ParcelProps = {
+    sbl: string;
+    swis: string;
+    address: string;
+};
+
+export default function Dashboard({ parcels }: { parcels: ParcelProps[] }) {
+    const [sorting, setSorting] = React.useState<SortingState>([]);
+
+    const columns = React.useMemo<ColumnDef<ParcelProps>[]>(
+        () => [
+            {
+                accessorKey: "sbl",
+                cell: (info) => info.getValue(),
+            },
+            {
+                accessorKey: "swis",
+                cell: (info) => info.getValue(),
+            },
+            {
+                accessorKey: "address",
+                cell: (info) => info.getValue(),
+            },
+        ],
+        []
+    );
+
+    const table = useReactTable({
+        data: parcels,
+        columns,
+        state: {
+            sorting,
+        },
+        onSortingChange: setSorting,
+        getSortedRowModel: getSortedRowModel(),
+        getCoreRowModel: getCoreRowModel(),
+    });
+
     return (
         <>
             <Head title="Dashboard" />
-            <div className="py-12">
-                <pre>{JSON.stringify(parcels, null, 2)}</pre>
+            <div className="container mx-auto py-12">
+                <div className="prose prose-slate">
+                    <table>
+                        <thead>
+                            {table.getHeaderGroups().map((headerGroup) => (
+                                <tr key={headerGroup.id}>
+                                    {headerGroup.headers.map((header) => (
+                                        <th
+                                            key={header.id}
+                                            colSpan={header.colSpan}
+                                        >
+                                            {header.isPlaceholder ? null : (
+                                                <div
+                                                    {...{
+                                                        className:
+                                                            header.column.getCanSort()
+                                                                ? "cursor-pointer select-none"
+                                                                : "",
+                                                        onClick:
+                                                            header.column.getToggleSortingHandler(),
+                                                    }}
+                                                >
+                                                    {flexRender(
+                                                        header.column.columnDef
+                                                            .header,
+                                                        header.getContext()
+                                                    )}
+                                                    {{
+                                                        asc: " 🔼",
+                                                        desc: " 🔽",
+                                                    }[
+                                                        header.column.getIsSorted() as string
+                                                    ] ?? null}
+                                                </div>
+                                            )}
+                                        </th>
+                                    ))}
+                                </tr>
+                            ))}
+                        </thead>
+                        <tbody>
+                            {table.getRowModel().rows.map((row) => (
+                                <tr key={row.id}>
+                                    {row.getVisibleCells().map((cell) => (
+                                        <td key={cell.id}>
+                                            {flexRender(
+                                                cell.column.columnDef.cell,
+                                                cell.getContext()
+                                            )}
+                                        </td>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </>
     );
